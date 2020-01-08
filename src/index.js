7b4128e80e1c1990123434abe47e57830cbf00b2
Clark Tyra <clark.tyra@gmail.com>
2020-01-08 15:04:16 -0800

    set up 3 persons on screen using same Person component but giving each different props info

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Person = ({img,name,job,children}) => {
+    const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
+    return (
+        <article>
+            <img 
+                src={url}
+                alt="person"
+            />
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+        </article>
+    )
+}
+
+
+
 const PersonList = () => {
-    return <section>my content will be here</section>
+    return (
+        <section>
+            <Person img="34" name="john" job="developer"/>
+            <Person img="24" name="bob" job="doctor"/>
+            <Person img="44" name="david" job="lawyer"/>
+        </section>
+    )
 }
 
 ReactDOM.render(<PersonList />, document.getElementById("root"));
