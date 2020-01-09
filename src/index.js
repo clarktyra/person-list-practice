9323facf5bcd2f63f4e58fcddb34107710ef4503
Clark Tyra <clark.tyra@gmail.com>
2020-01-08 17:05:34 -0800

    added styling to the page, used box-shadow with a transition of .3 sec to give each component a pop when hovered, also made each icon image circular by making img border radius 50%

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css'
 
 const Person = ({img,name,job,children}) => {
     const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
     return (
-        <article>
-            <img 
-                src={url}
-                alt="person"
-            />
+        <article className="person">
+            <img src={url}alt="person"/>
             <h4>{name}</h4>
             <h4>{job}</h4>
+            {children}
         </article>
     )
 }
@@ -19,9 +18,11 @@ const Person = ({img,name,job,children}) => {
 
 const PersonList = () => {
     return (
-        <section>
+        <section className="person-list">
             <Person img="34" name="john" job="developer"/>
-            <Person img="24" name="bob" job="doctor"/>
+            <Person img="24" name="bob" job="doctor">
+            <p>Lorem20 Lorem20 Lorem20 Lorem2 0Lorem20 Lorem20 Lorem20 Lorem20 Lorem20 Lorem20</p>
+            </Person>
             <Person img="44" name="david" job="lawyer"/>
         </section>
     )
